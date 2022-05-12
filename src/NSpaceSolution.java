e1516309af4965fce985bff855e0f9cd6dbb7f38
Aliaa Fathy <94761307+AliaaFathy@users.noreply.github.com>
2022-05-12 19:29:50 +0200

    Update NSpaceSolution.java

--- src/NSpaceSolution.java
+++ src/NSpaceSolution.java
@@ -15,7 +15,7 @@ public class NSpaceSolution extends HashedSet {
         this.secondLevel = new ArrayList[n];
         this.isFull = new boolean[n];
         this.HashFunctions = new ArrayList[n];
-        this.hashFunction = new int[this.b][this.u];
+        this.hashFunctionOfMethod2 = new int[this.b][this.u];
         this.secondHashTable = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             secondLevel[i] = new ArrayList<>();
@@ -32,17 +32,19 @@ public class NSpaceSolution extends HashedSet {
 
 
     private ArrayList<Integer>[] generateFirstLevel() {
-        hashFunction = generateRandomH(this.b, this.u);
+        /**...Generate Random Hash Function**/
+        hashFunctionOfMethod2 = generateRandomH(this.b, this.u);
         for (int i = 0; i < this.n; i++) {
             boolean flag = false;
             while (!flag) {
                 int[] x = getBinaryRep(this.S[i]);
-                int[] binaryIndex = multiplyMatrices(hashFunction, x);
+                int[] binaryIndex = multiplyMatrices(hashFunctionOfMethod2, x);
                 int index = getIndex(binaryIndex);
+                /**If there is no collision put this number in first level hash**/
                 if (!this.isFull[index]) {
                     this.output[index] = this.S[i];
                     this.isFull[index] = true;
-                } else {
+                } else { //else put the number made collision in array of number that made collision with the same number
                     this.secondLevel[index].add(this.S[i]);
                 }
                 flag = true;
@@ -50,7 +52,7 @@ public class NSpaceSolution extends HashedSet {
         }
         return secondLevel;
     }
-
+       /**To generate second level Hash**/
     public void generateOutput() {
         ArrayList<Integer>[] firstLevel = generateFirstLevel();
         for (int i = 0; i < this.n; i++) {
@@ -59,9 +61,11 @@ public class NSpaceSolution extends HashedSet {
                 for (int j = 0; j < firstLevel[i].size(); j++) {
                     newInput[j] = firstLevel[i].get(j);
                 }
-
+                /**Apply first method on the vector of collision**/
                 NSquaredSpaceSolution finalSolution = new NSquaredSpaceSolution(newInput);
+                /**Store Hash function of Each Vector**/
                 this.HashFunctions[i].add(finalSolution.getHashFunction());
+                /**Store second Hash Table of Each Vector **/
                 secondHashTable[i].add(finalSolution.getOutput());
 
             }
@@ -69,20 +73,21 @@ public class NSpaceSolution extends HashedSet {
     }
 
     public void lookUp(int element) {
-        int index;
+        int i;
         int[] binaryInd;
         int[] x = getBinaryRep(element);
-        binaryInd = multiplyMatrices(hashFunction, x);
-        index = getIndex(binaryInd);
+        binaryInd = multiplyMatrices(hashFunctionOfMethod2, x);
+        i = getIndex(binaryInd); // i=h(x)
         int[] secondBinaryIndex;
-        if (this.output[index] == element) {
+        /**check if the element in the first hash level**/
+        if (this.output[i] == element) {
             System.out.println("Found in first level.");
-        } else {
-            if (!this.HashFunctions[index].isEmpty()) {
-                secondBinaryIndex = multiplyMatrices(this.HashFunctions[index].get(0), x);
+        } else {//if not check in the second hash level
+            if (!this.HashFunctions[i].isEmpty()) {
+                secondBinaryIndex = multiplyMatrices(this.HashFunctions[i].get(0)/*hi*/, x);//hi(x)
                 int secondIndex = getIndex(secondBinaryIndex);
-                int[] out = secondHashTable[index].get(0);
-                if (out[secondIndex] == element) {
+                int[] out = secondHashTable[i].get(0);//Ai
+                if (out[secondIndex] == element) {//Ai[hi(x)]
                     System.out.println("Found in second level.");
                 } else {
                     System.out.println("Not Found!");
