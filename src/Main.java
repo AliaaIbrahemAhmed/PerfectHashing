10076c338ab4e4038642d47a5cba735c3eca2620
Aliaa Fathy <94761307+AliaaFathy@users.noreply.github.com>
2022-05-12 21:46:09 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] S = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        int[] S = new int[]{1, 2, 3, 4, 5, 6, 7,8,9,10,50,60,70,80,100,1000,10000};
         NSquaredSpaceSolution nSquaredSpaceSolution = new NSquaredSpaceSolution(S);
+        System.out.println("number of rebuild hash of method 1= "+nSquaredSpaceSolution.counter);
         NSpaceSolution nSpaceSolution = new NSpaceSolution(S);
         for(int i : S){
             nSquaredSpaceSolution.lookUp(i);
