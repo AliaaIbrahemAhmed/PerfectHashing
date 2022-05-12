e12d00914bb27d49c3b16d34c478a92d084c9c04
Aliaa Fathy <94761307+AliaaFathy@users.noreply.github.com>
2022-05-12 03:22:18 +0200

    Add files via upload

---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int[] S = new int[]{1,2,3,4,5,6,7,8};
+        NSpaceSolution NSpaceSolution = new NSpaceSolution(S);
+        NSpaceSolution.lookup(8);
+    }
+}
